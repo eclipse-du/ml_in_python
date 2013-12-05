dcdc92f1fc8292b386e5fdd5ee56aba8ef841af4
eclipse-du <adooadoo@163.com>
2013-12-05 15:57:49 +0800

    update file

--- Linear_Regression/Linear_Regression_plt.py
+++ Linear_Regression/Linear_Regression_plt.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import numpy as np
 import sys
