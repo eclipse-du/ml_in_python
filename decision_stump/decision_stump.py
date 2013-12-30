df0f241dae79556a9a766f3439dadde7922b269b
eclipse-du <adooadoo@163.com>
2013-12-30 21:57:37 +0800

    test

--- decision_stump/decision_stump.py
+++ decision_stump/decision_stump.py
@@ -1,9 +1,32 @@
-import operator
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-data =[]
-with open('rawdata_ds.txt') as f:
-    s = [[float(x) for x in line.strip('\n').split(',')] for line in f]
-    
-s.sort(key=lambda x:x[0]) 
-for (a,b) in s:
-    print a,b
+# author  : eclipse
+# email   : adooadoo@163.com
+
+
+def load_data(input_file):
+    """
+    Description:
+    load from input_file,spliting with ','
+    take it into array
+    ****************************************
+    Parameters:
+    input_file   :data source
+    ****************************************
+    Return:
+    data:data in the type of List
+    """
+
+    data = []
+    with open(input_file) as f:
+        data = [[float(x) for x in line.strip('\n').split(',')]
+                for line in f]
+    return data
+
+
+if __name__ == '__main__':
+    data = load_data('rawdata_ds.txt')
+    data.sort(key=lambda x: x[0])
+    for (x, y) in data:
+        print x, y
