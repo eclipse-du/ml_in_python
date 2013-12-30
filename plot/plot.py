f980662fa4b0c0e061a549c77caece7c277a43c4
eclipse-du <adooadoo@163.com>
2013-12-30 14:46:27 +0800

    delete sth

--- plot/plot.py
+++ plot/plot.py
@@ -12,7 +12,7 @@ X = np.array([[1,2],[2,2],[1,1],[0,0],[-1,1],[2,-1]])
 y = np.array([1,1,1,-1,-1,-1])
 pos = np.array([X[i] for i in range(np.size(y)) if y[i]==1])
 neg = np.array([X[i] for i in range(np.size(y)) if y[i]==-1])
-#print y1
+
 #y2 = np.array([0 if i==1 else 1 for i in y])
 pl.axis([-4,4,-4,4])
 pl.plot(pos[:,0],pos[:,1],'ro',label='positve',markersize=10)
