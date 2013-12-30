99b071cccb7bde06c4776a873190d0e4c6d69fc9
eclipse-du <adooadoo@163.com>
2013-12-30 22:53:12 +0800

    new

--- decision_stump/decision_stump.py
+++ decision_stump/decision_stump.py
@@ -24,9 +24,33 @@ def load_data(input_file):
                 for line in f]
     return data
 
+def create_Theta(data):
+	"""
+    Description:
+    Create Theta using given data
+    ****************************************
+    Parameters:
+    data   :data source(list)
+    ****************************************
+    Return:
+    thetas:all theta in the type of List
+    """
+	thetas = [data[0][0] - 0.01] + [(data[i][0] + data[i + 1][0]) / 2
+                                   for i in range(0, len(data) - 1)] \
+        + [data[len(data) - 1][0] + 0.01]
+	return thetas
 
 if __name__ == '__main__':
     data = load_data('rawdata_ds.txt')
+    size = len(data)    
     data.sort(key=lambda x: x[0])
-    for (x, y) in data:
-        print x, y
+    for x,y in data:
+    	print x,y
+    thetas = create_Theta(data)
+    tmp = [sum([1 for i in xrange(0,size) if data[i][0]<thetas[j] and data[i][1]==-1 or data[i][0]>thetas[j] and data[i][1]==1]) for j in xrange(0,len(thetas)) ]
+    t_max = max(tmp)
+    t_min = min(tmp)
+    if (t_max>=size-t_min):
+    	print thetas[tmp.index(t_max)],1,(size-t_max)/(size*1.0)
+    else:
+    	print thetas[tmp.index(t_min)],-1,t_min/size
