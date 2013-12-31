a651f0a2d2d391a3eb5701bc118193a1e38fd205
eclipse-du <adooadoo@163.com>
2013-12-31 11:56:34 +0800

    fixed bug

--- decision_stump/decision_stump.py
+++ decision_stump/decision_stump.py
@@ -6,7 +6,8 @@
 
 import os
 
-def load_data(input_file):
+
+def load_data(input_file, regex=None):
     """
     Description:
     load from input_file,spliting with ','
@@ -20,13 +21,16 @@ def load_data(input_file):
     """
 
     data = []
+    if regex == None:
+        regex = ','
     with open(input_file) as f:
-        data = [[float(x) for x in line.strip('\n').split(',')]
-                for line in f]
+        data = [[float(x) for x in line.strip(' ').strip('\n'
+                ).split(regex)] for line in f]
     return data
 
+
 def create_Theta(data):
-	"""
+    """
     Description:
     Create Theta using given data
     ****************************************
@@ -36,43 +40,97 @@ def create_Theta(data):
     Return:
     thetas:all theta in the type of List
     """
-	thetas = [data[0][0] - 0.01] + [(data[i][0] + data[i + 1][0]) / 2
-                                   for i in range(0, len(data) - 1)] \
+
+    thetas = [data[0][0] - 0.01] + [(data[i][0] + data[i + 1][0]) / 2
+                                    for i in range(0, len(data) - 1)] \
         + [data[len(data) - 1][0] + 0.01]
-	return thetas
+    return thetas
 
-def decisio_stump_simple(data):
-	"""
-	Description:
-	Create Theta using given data
-	****************************************
-	Parameters:
-	data   :data source(list)
-	****************************************
-	Return:
-	thetas:all theta in the type of List
-	"""
 
-	size = len(data)    
-	data.sort(key=lambda x: x[0])
-	thetas = create_Theta(data)
-	tmp = [sum([1 for i in xrange(0,size) if data[i][0]<thetas[j] and data[i][1]==-1 or data[i][0]>thetas[j] and data[i][1]==1]) for j in xrange(0,len(thetas)) ]
-	t_max = max(tmp)
-	t_min = min(tmp)
-	if (t_max>=size-t_min):
-		return [thetas[tmp.index(t_max)],1,(size-t_max)/(size*1.0)]
-	else:
-		return [thetas[tmp.index(t_min)],-1,t_min/size]
+def find_median_value_index(list_data, value):
+    """
+    Description:
+    Get given value in list_data 
+    return the median one's index
+    ****************************************
+    Parameters:
+    list_data   :data source(list)
+    value       :value in data
+    ****************************************
+    Return:
+    index:the median value's index
+    """
 
-if __name__ == '__main__':
-    #data = load_data('rawdata_ds.txt')
+    count = list_data.count(value)
+    count = count / 2 + 1
+    step = 1
+    for i in xrange(0, len(list_data)):
+        if list_data[i] == value:
+            if step == count:
+                return i
+            step += 1
+
+
+def decisio_stump_single(data):
+    """
+....Description:
+....Create Theta using given data
+....****************************************
+....Parameters:
+....data   :data source(list)
+....****************************************
+....Return:
+....thetas:all theta in the type of List
+...."""
+
+    size = len(data)
+    data.sort(key=lambda x: x[0])
+    thetas = create_Theta(data)
+    tmp = [sum([1 for i in xrange(0, size) if data[i][0] < thetas[j]
+           and data[i][1] == -1 or data[i][0] > thetas[j]
+           and data[i][1] == 1]) for j in xrange(0, len(thetas))]
+    t_max = max(tmp)
+    t_min = min(tmp)
+    if t_max >= size - t_min:
+        index = find_median_value_index(tmp, t_max)
+        return [thetas[index], 1, (size - t_max) / (size * 1.0)]
+    else:
+        index = find_median_value_index(tmp, t_min)
+        return [thetas[index], -1, t_min / size]
+
+
+def test_single_ds():
+    """
+    Description:
+    Testing my method to implement the
+    algorithm of single variable sc
+    ****************************************
+    Parameters:
+    None
+    ****************************************
+    Return:
+    ein   : error in the case of ds
+    eout  : error of the real data case
+    """
+
     ein = 0.0
     eout = 0.0
     for data_file in os.listdir('data'):
-    	data = load_data('data/'+data_file)
-    	ans = decisio_stump_simple(data)
+        data = load_data('data/' + data_file)
+        ans = decisio_stump_single(data)
         ein += ans[2]
-        eout += 0.3*ans[1]*(abs(ans[0])-1)
+        eout += 0.3 * ans[1] * (abs(ans[0]) - 1)
     size = len(os.listdir('data'))
-    print ein/size
-    print (eout+size/2.0)/size
+    return (ein / size, (eout + size / 2.0) / size)
+
+
+def test_multi_ds():
+    data = load_data('multi_sc_train.txt', ' ')
+    for i in data:
+        print i
+
+
+if __name__ == '__main__':
+    print test_single_ds()
+
+    # test_multi_ds()
